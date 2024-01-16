bc2fa7ff567e20dbd15e5ae8361bef6944e6aa6f
sdac-1 <sdac-1@sdac-1-PC>
2024-01-16 09:37:43 +0530

    gp v2.0

--- gitpract2/src/gitpract2/gpract2.java
+++ gitpract2/src/gitpract2/gpract2.java
@@ -3,7 +3,8 @@ package gitpract2;
 public class gpract2 {
 
 	public static void main(String[] args) {
-		System.out.println("gp2 v.10");
+		System.out.println("gp2 v1.0");
+		System.out.println("gp2 v2.0");
 
 	}
 
